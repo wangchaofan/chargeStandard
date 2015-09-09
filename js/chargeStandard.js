6af479ff68113a5b3bb3f3a9711b91f45d50fa2a
wangchaofan <514610423@qq.com>
2015-09-09 20:35:08 +0800

    complish

--- js/chargeStandard.js
+++ js/chargeStandard.js
@@ -1,13 +1,52 @@
+var Charge = {
+	totalPrice: 0
+};
+var $btnRegistUer = $('.btn-regist-user');
+
+var $priceVal = $('.expenses .price'); 
+
+//选择服务
 $('.rc').on('click', '.span1, .span2', function () {
 	$(this).siblings().removeClass('select');
 	$(this).addClass('select');
+	$('#' + $(this).parent().data('select')).text($(this).text());
+	Charge.balance();
+});
+
+$('#protocol').on('click', function () {
+	if($(this).is(':checked')) {
+		$btnRegistUer.removeClass('disabled');
+	} else {
+		$btnRegistUer.addClass('disabled');
+	}
 });
 
 var jianliScroll = new Scroll({
 	render: 'jianLi',
 	sections: [100, 200, 500, 1000, 3000],
 	danwei: '封',
-	origValue: 100,
-	step: 100,
+	origValue: 200,
+	step: 10,
 	unitPrice: 20
 });
+
+var zhaopinScroll = new Scroll({
+	render: 'zhaoPin',
+	sections: [1, 2, 5, 8, 10],
+	danwei: '场',
+	origValue: 2,
+	step: 1,
+	unitPrice: 20
+});
+// 计算价格
+Charge.balance = function () {
+	Charge.totalPrice = 0;
+	$priceVal.text('正在计算价格..');
+	$('.select').each(function () {
+		Charge.totalPrice += parseFloat($(this).attr('price'));
+	});
+	Charge.totalPrice = Charge.totalPrice.toFixed(2);
+	$priceVal.text(Charge.totalPrice);
+}
+//初始计算价格
+Charge.balance();
--- js/scroll.js
+++ js/scroll.js
@@ -9,18 +9,20 @@ var Scroll = function (options) {
 	this.dom = null;
 	$.extend(this.options, options);
 	this.value = this.options.origValue;
-	this.coverLength = this.getCoverLength();
-	console.log(this.coverLength);
+	this.price = this.options.origValue / this.options.step * this.options.unitPrice;
+	this.coverLength = 0;
 	this.init();
 }
 Scroll.prototype = {
 	init: function () {
+		var _self = this;
 		var options = this.options;
 		this.dom    = $('#' + options.render);
 		if(this.dom.length < 0) {
 			throw new Error(0, 'render is not found');
 		}
-		this.dom.addClass('scroll');
+		this.dom.addClass('scroll').attr('price', _self.price);
+		this.setCoverLength();
 		this.initHtml();
 		this.initEvent();
 	},
@@ -28,37 +30,58 @@ Scroll.prototype = {
 		var _self   = this,
 			options = this.options;
 		var $rangeScroll = $('<div class="scroll-range">');
+		var left = _self.coverLength + 'px';
 		var sections_html = [];
 		for(var i = 0;i < options.sections.length; i++) {
 			sections_html.push('<span class="scroll-item-box" data-value="'+ options.sections[i] +'">'+ options.sections[i] +'</span>')
 		}
-		var scroll_cover_html = '<div class="scroll-cover scroll-transition"><div class="scroll-current">' + sections_html.join('') + 
+		var scroll_cover_html = '<div class="scroll-cover scroll-transition" style="width:'+ left +'"><div class="scroll-current">' + sections_html.join('') + 
 								'</div></div>';
 		var scroll_bar_html   = '<div class="scroll-bar">' + sections_html.join('') + '</div>';
-		var scroll_thum_html  = '<div class="scroll-thumb scroll-transition"><span></span><span></span></div>';
+		var scroll_thum_html  = '<div class="scroll-thumb scroll-transition" style="left:'+ left +'"><span></span><span></span></div>';
 		var scroll_value_html = '<div class="scroll-value nostyle">' +
-									'<input type="number" value="'+ options.origValue +'" step="'+ options.step +'"/>' +
+									'<input type="number" value="'+ options.origValue +'" step="'+ options.step +'" min="'+ options.step +'" max="'+ options.sections[i-1] +'"/>' +
 									'<p class="danwei">'+ options.danwei +'</p>' +
 								'</div>';
 		$rangeScroll.html(scroll_thum_html + scroll_cover_html + scroll_bar_html).appendTo(_self.dom);
 		_self.dom.append(scroll_value_html);
 	},
 	initEvent: function () {
-		var startX = 0,
-			down   = false;
-			left   = 0;
+		var _self = this;
+		const RE = /^\d+/;
 		var $thumb = this.dom.find('.scroll-thumb');
 		var $cover = this.dom.find('.scroll-cover');
 		var $range = this.dom.find('.scroll-range');
+		var $input = this.dom.find('input');
+		var startX = 0,
+			down   = false;
+			rangeLength = parseInt(RE.exec($range.css('width')), 10) - 16;
+
+		//点击选择
 		$range.on('mousedown', function (event) {
 			var event = event || window.event;
 			var pageX = event.pageX;
 			var offsetX = $range.offset().left;
-			$thumb.css('left', pageX - offsetX);
-			$cover.css('width', pageX - offsetX);
+			_self.coverLength = Math.floor(pageX - offsetX > rangeLength ? rangeLength : pageX - offsetX);
+			$thumb.css('left',  _self.coverLength);
+			$cover.css('width', _self.coverLength);
+			_self.setValue();
+			_self.setCoverLength();
 		});
+		//直接输入
+		$input.on('change', function () {
+			var $this = $(this);
+			var min = parseInt($this.attr('min'), 10);
+			var max = parseInt($this.attr('max'), 10);
+			var value = $this.val();
+			value = value < min ? min : (value > max ? max : value);
+			value = Math.ceil(value / _self.options.step) * _self.options.step;
+			$this.val(value);
+			_self.value = value;
+			_self.setCoverLength();
+		});
+		//滑动选择
 		$thumb.bind('mousedown', mouseDown);
-		$('body').bind('mouseup', mouseUp);
 
 		function mouseDown(event) {
 			var event = event || window.event;
@@ -68,34 +91,71 @@ Scroll.prototype = {
 			$cover.removeClass('scroll-transition');
 			$thumb.removeClass('scroll-transition');
 			$('body').bind('mousemove', mouseMove);
+			$('body').bind('mouseup', mouseUp);
 			return false;
 		}
 		function mouseMove(event) {
-			if(down) {
-				var event       = event || window.event;
-				var slideLength = (event.pageX - startX + left) > 0 ? (event.pageX - startX + left) : 0;
-				$thumb.css('left',  slideLength);
-				$cover.css('width', slideLength);
+			if(!down) return false;
+			var event       = event || window.event;
+			var slideLength = event.pageX - startX + _self.coverLength;
+			if(slideLength < 0) {
+				slideLength = 0;
+			} else if(slideLength > rangeLength) {
+				slideLength = rangeLength;
 			}
+			$thumb.css('left',  slideLength);
+			$cover.css('width', slideLength);
+			_self.setValue(slideLength);
 		}
 		function mouseUp(event) {
+			if(!down) return false;
 			down = false;
-			left = parseInt($thumb.css('left').substr(0, $thumb.css('left').length - 2), 10);
 			$cover.addClass('scroll-transition');
 			$thumb.addClass('scroll-transition');
 			$('body').unbind('mousemove');
+			$('body').unbind('mouseup');
+			_self.setCoverLength();
 		}
 	},
-	setValue: function () {
-		setTimeout()
+	//set值
+	setValue: function (slideLength) {
+		var _self = this;
+		var sections = this.options.sections;
+		var length = slideLength || _self.coverLength;
+		var i = Math.floor(length / 100),
+			l = (length - 100 * i)%100;
+
+		if(i === 0) {
+			_self.value = _self.options.step;
+		} else if(i >= sections.length) {
+			_self.value = sections[i-1];
+		} else {
+			_self.value = (sections[i-1] || _self.options.step) + ((sections[i] || 0) - sections[i-1]) * l / 100;
+			_self.value = _self.value - _self.value % _self.options.step + _self.options.step;
+		}
+		_self.setPrice();
+		_self.dom.find('input').val(_self.value);
 	},
-	getCoverLength: function () {
+	//set覆盖范围
+	setCoverLength: function () {
 		var _self    = this;
+		var $thumb = this.dom.find('.scroll-thumb');
+		var $cover = this.dom.find('.scroll-cover');
 		var sections = this.options.sections;
 		var i = 0;
 		while(sections[i] < _self.value) {
 			i++;
 		}
-		return  100 * i + (_self.value - (sections[i - 1] || 0)) / (sections[i] / 100);
+		_self.coverLength = Math.floor(100 * i + (_self.value - (sections[i - 1] || 0)) / ((sections[i] - (sections[i - 1] || 0)) / 100));
+		//console.log(_self.coverLength);
+		$thumb.css('left',  _self.coverLength);
+		$cover.css('width', _self.coverLength);
+	},
+	//set价格
+	setPrice: function () {
+		var _self = this;
+		_self.price = _self.value / _self.options.step * _self.options.unitPrice;
+		_self.dom.attr('price', _self.price);
+		Charge.balance();
 	}
 }
